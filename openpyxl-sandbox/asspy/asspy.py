85314c96c92a99cdd001fd19ee52edea8e3b7ee0
assout <assout@users.noreply.github.com>
2019-04-08 00:50:05 +0900

    WIP:  openpyxl-sandbox/Pipfile.lock        |   6 +++--  openpyxl-sandbox/asspy/asspy.py      |  43 ++++++++++++++++++++++++++++++-----  openpyxl-sandbox/asspy/example.xlsx  | Bin 4539 -> 0 bytes  openpyxl-sandbox/asspy/input.xlsx    | Bin 0 -> 5462 bytes  openpyxl-sandbox/asspy/new.xlsx      | Bin 0 -> 5550 bytes  openpyxl-sandbox/asspy/template.xlsx | Bin 0 -> 5550 bytes  6 files changed, 41 insertions(+), 8 deletions(-)

--- openpyxl-sandbox/asspy/asspy.py
+++ openpyxl-sandbox/asspy/asspy.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8 :
 import openpyxl
+import shutil
+import argparse
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class SourceRecord:
+    id: int
+    name: str
+    is_oridinal: bool = False
+
+    def bow(self) -> str:
+        return self.name + ' めー'
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input')
+    parser.add_argument('-o', '--output')
+    args = parser.parse_args()
+    print(args.input)
+
+    shutil.copyfile("./template.xlsx", "new.xlsx")
+    wb = openpyxl.load_workbook('new.xlsx')
+    ws = wb["Sheet"]
+    print(ws)
+    print(tuple(ws))
+    # s['A1:']
+
+    # i = openpyxl.utils.cell.column_index_from_string("b")
+    # print(i)
+
+    record = SourceRecord(1, "りんご")
+    print(record.bow())
+
 
 if __name__ == '__main__':
-    """
-    """
-    wb = openpyxl.Workbook()
-
-    # 保存
-    wb.save('example.xlsx')
+    main()

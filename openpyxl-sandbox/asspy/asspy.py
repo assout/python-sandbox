0728f9da2d67bbdb6cebfd59c7a99afa5f611ef2
assout <assout@users.noreply.github.com>
2019-04-07 09:32:42 +0900

    WIP:  openpyxl-sandbox/Pipfile             | 10 ++++  openpyxl-sandbox/Pipfile.lock        | 91 ++++++++++++++++++++++++++++++++++++  openpyxl-sandbox/README.md           |  3 ++  openpyxl-sandbox/asspy/asspy.py      |  4 ++  openpyxl-sandbox/tests/test_asspy.py | 12 +++++  5 files changed, 120 insertions(+)

---
+++ openpyxl-sandbox/asspy/asspy.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+print(1)
---
+++ openpyxl-sandbox/tests/test_asspy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# vim: set fileencoding=utf-8 :
+
+from unittest import TestCase
+from asspy.asspy import Foo
+
+class TestFoo(TestCase):
+
+    def test_say(self):
+        self.assertEqual(foo.say(), 'foo')
+
